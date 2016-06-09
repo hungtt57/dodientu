cf533b619d7efae38178ef8b0a01e982da0be6f3
trịnh huy <trinhhuyk57>
2016-06-09 10:38:10 +0700

    update migration

---
+++ database/migrations/2016_06_07_101826_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('alias');
+            $table->text('description');
+            $table->text('short_description');
+            $table->text('info');
+            $table->string('image');
+            $table->integer('new_price');
+            $table->integer('old_price');
+            $table->string('sale');
+            $table->integer('category_id')->unsigned();
+            $table->text('more');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('products');
+    }
+}
